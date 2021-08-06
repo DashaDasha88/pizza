fef6021adc3ba54c7157fafec31c215d3058c857
Dasha <dasha.kotova.mail@gmail.com>
2021-08-06 18:05:40 -0400

    add pizza icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   )
 }
 
