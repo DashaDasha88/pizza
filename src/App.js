5891a4b33692f2d1ea08d55b1b118b0496154cda
Dasha <dasha.kotova.mail@gmail.com>
2021-08-09 19:34:03 -0400

    add hero image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
