9c49ccaa3fcc9bb6a818d4b3ec2fce8356f23ca3
Dasha <dasha.kotova.mail@gmail.com>
2021-08-11 13:06:03 -0400

    add imports

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+} from "./SidebarElements";
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
+
       <Icon>
         <CloseIcon />
       </Icon>
+
       <SidebarMenu>
         <SidebarLink to="/">Pizzas</SidebarLink>
         <SidebarLink to="/">Desserts</SidebarLink>
         <SidebarLink to="/">Full Menu</SidebarLink>
       </SidebarMenu>
+
+      <SideBtnWrap>
+        <SidebarRoute to="/">Order Now</SidebarRoute>
+      </SideBtnWrap>
+
     </SidebarContainer>
   )
 }
