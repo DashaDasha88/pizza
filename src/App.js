cd6bbb4dce74f4cd83082f531fd60c54741b8eb1
Dasha <dasha.kotova.mail@gmail.com>
2021-08-13 18:25:45 -0400

    add feature styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Products from './components/Products';
+import Feature from './components/Feature';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import { productData } from "./components/Products/data";
@@ -11,6 +12,7 @@ const App = () => {
       <GlobalStyle />
       <Hero />
       <Products heading="Choose your favourite" data={productData} />
+      <Feature />
     </Router>
   )
 }
---
+++ src/components/Feature/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>Pizza of the Day</h1>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  )
+}
+
+export default Feature;
